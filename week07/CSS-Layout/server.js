7d4ce313978e5a14e4e7966ababbc1ab7a5fdb9f
leo lee <1733132278@qq.com>
2020-05-27 22:07:51 +0800

    Add week07

---
+++ week07/CSS-Layout/server.js
@@ -0,0 +1,34 @@
+const http = require("http");
+// Returns content-type = text/plain
+const server = http.createServer((req, res) => {
+    console.log("request received");
+    console.log(req.headers["x-foo2"]);
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('X-Foo', 'bar');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(`
+<html maaa=a>
+<head>
+    <style>
+    #container {
+        width: 600px;
+        height: 300px;
+        display: flex;
+    }
+    #container #myid{
+        width: 200px;
+    }
+    #container .c1{
+        flex: 1;
+    }
+    </style>
+</head>
+<body>
+    <div id="container">
+        <div id="myid" />
+        <div class="c1" />
+    </div>
+</body>
+</html>`);
+});
+server.listen(8088);
